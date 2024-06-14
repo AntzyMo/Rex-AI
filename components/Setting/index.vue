2ada69376a5d8c4230321261976fa6386a6af8d0
AntzyMo <mozbano@163.com>
2024-06-14 16:37:38 +0800

    chore: init

---
+++ components/Setting/index.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="w-xs font-mono pr-4">
+    <div>
+      <h1 class="mb-1">
+        Model
+      </h1>
+      <div border-t="~ gray-200" class="pt-2">
+        <div class="text-sm mb-4" flex="~ items-center gap-2">
+          <div i-simple-icons:microsoftazure class="text-sm" />
+          Azure OpenAI
+        </div>
+        <div class="grid gap-3">
+          <Input type="password" title="API Key" />
+          <Input title="Resource Name" />
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -5,13 +5,16 @@
 <template>
   <div class="h-100vh py-3">
     <NavBar />
-    <Splitpanes class="h-95%">
-      <Pane>
-        <MatchArea />
-      </Pane>
-      <Pane size="40">
-        <AIArea />
-      </Pane>
-    </Splitpanes>
+    <div flex="~ 1" class="h-95%">
+      <Splitpanes>
+        <Pane>
+          <MatchArea />
+        </Pane>
+        <Pane size="40">
+          <AIArea />
+        </Pane>
+      </Splitpanes>
+      <Setting />
+    </div>
   </div>
 </template>
