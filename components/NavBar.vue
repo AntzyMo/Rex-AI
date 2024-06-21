39133a210378f717c51f86c280f68d9f9867e7dc
AntzyMo <mozbano@163.com>
2024-06-21 09:33:40 +0800

    chore:  navBar title

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -2,7 +2,28 @@
 </script>
 
 <template>
-  <nav class="mx-5 py-3">
-    <h1>Rex AI</h1>
+  <nav class="mx-5  pb-3 font-mono" flex="~ items-start justify-between">
+    <div class="" flex="~ items-center gap-1">
+      <span i-tabler:regex class="text-gray-400 text-xl" />
+      <div class="mt-2">
+        <h1 class="text-xs self-end">
+          Rex AI
+        </h1>
+      </div>
+    </div>
+    <div class="text-10px text-end text-gray-400">
+      <div flex="~ items-center justify-end">
+        <a
+          i-carbon:logo-github
+          href="https://github.com/AntzyMo/Rex-AI"
+          target="_blank"
+          class="text-xs cursor-pointer hover:text-gray-500"
+        />
+      </div>
+      <div class="select-none">
+        <p>© 2024-PRESENT</p>
+        <p>Made By AntzyMo </p>
+      </div>
+    </div>
   </nav>
 </template>
